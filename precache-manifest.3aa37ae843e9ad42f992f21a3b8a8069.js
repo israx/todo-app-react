5f7684a0429dc558c76771e5bf54814bc9761ab2
Israel <arcos@israels-mbp.lan>
2020-09-12 17:05:12 -0400

    Updates

---
+++ precache-manifest.3aa37ae843e9ad42f992f21a3b8a8069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c673517aeba95cd751f1095e1e3aa6",
+    "url": "/israx/todo-app-react/index.html"
+  },
+  {
+    "revision": "c78f43884219c15a46e1",
+    "url": "/israx/todo-app-react/static/css/main.9468aa05.chunk.css"
+  },
+  {
+    "revision": "3f76bfd968581874d475",
+    "url": "/israx/todo-app-react/static/js/2.ff977aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/israx/todo-app-react/static/js/2.ff977aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78f43884219c15a46e1",
+    "url": "/israx/todo-app-react/static/js/main.b8ab0b88.chunk.js"
+  },
+  {
+    "revision": "2320859e271c8151fe4b",
+    "url": "/israx/todo-app-react/static/js/runtime-main.62e9ca79.js"
+  }
+]);
